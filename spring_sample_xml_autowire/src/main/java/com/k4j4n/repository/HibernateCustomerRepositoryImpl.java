a842ab0e00661bbe9a67c05e8173d5b09fabefcf
KajanM <kajan.14@cse.mrt.ac.lk>
2017-08-06 15:23:42 +0530

    dependency injection | xml configuration| autowire | constructor

---
+++ spring_sample_xml_autowire/src/main/java/com/k4j4n/repository/HibernateCustomerRepositoryImpl.java
@@ -0,0 +1,26 @@
+package com.k4j4n.repository;
+
+import com.k4j4n.model.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by K4J4N on 8/6/2017.
+ */
+public class HibernateCustomerRepositoryImpl implements CustomerRepository {
+
+    @Override
+    public List<Customer> findAll() {
+        List<Customer> customers = new ArrayList<>();
+
+        Customer customer = new Customer();
+
+        customer.setFistName("Kajan");
+        customer.setLastName("Sivam");
+
+        customers.add(customer);
+
+        return customers;
+    }
+}
---
+++ spring_sample_xml_autowire/src/main/java/com/k4j4n/service/CustomerServiceImpl.java
@@ -0,0 +1,29 @@
+package com.k4j4n.service;
+
+import com.k4j4n.model.Customer;
+import com.k4j4n.repository.CustomerRepository;
+
+import java.util.List;
+
+/**
+ * Created by K4J4N on 8/6/2017.
+ */
+public class CustomerServiceImpl implements CustomerService {
+
+    private CustomerRepository customerRepository;
+
+    public CustomerServiceImpl() {
+
+    }
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+
+}
