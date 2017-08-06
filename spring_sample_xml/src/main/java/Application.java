dcc7a5a8831d45c0e8b9d4f5219da43701d6c804
KajanM <kajan.14@cse.mrt.ac.lk>
2017-08-06 14:32:19 +0530

    setter injection with xml configuration

---
+++ spring_sample_xml/src/main/java/Application.java
@@ -0,0 +1,22 @@
+import com.k4j4n.service.CustomerService;
+import com.k4j4n.service.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.applet.AppletContext;
+
+/**
+ * Created by K4J4N on 8/6/2017.
+ */
+public class Application {
+
+    public static void main(String[] args) {
+
+        //CustomerService customerService = new CustomerServiceImpl();
+
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("appContext.xml");
+
+        CustomerService service = appContext.getBean("customerService", CustomerService.class);
+        System.out.println(service.findAll().get(0).getFistName());
+    }
+}
