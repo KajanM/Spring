00c3e72dcddb6d481a35e01a47ef98f2de961cc9
KajanM <kajan.14@cse.mrt.ac.lk>
2017-08-06 18:59:33 +0530

    dependency injection | xml configuration | autowire | byName

---
+++ spring_sample_xml_autowire_byName/src/main/java/com/k4j4n/service/CustomerService.java
@@ -0,0 +1,12 @@
+package com.k4j4n.service;
+
+import com.k4j4n.model.Customer;
+
+import java.util.List;
+
+/**
+ * Created by K4J4N on 8/6/2017.
+ */
+public interface CustomerService {
+    List<Customer> findAll();
+}
