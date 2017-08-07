b93e2fa23ec5ef48cc3d2bb5a42c696504c877b7
KajanM <kajan.14@cse.mrt.ac.lk>
2017-08-07 08:34:57 +0530

    dependency injection | xml configuration | annotation | setter injection

---
+++ spring_sample_xml_annotation_setter/src/main/java/com/k4j4n/service/CustomerServiceImpl.java
@@ -0,0 +1,30 @@
+package com.k4j4n.service;
+
+import com.k4j4n.model.Customer;
+import com.k4j4n.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by K4J4N on 8/6/2017.
+ */
+
+@Service("customerService")
+public class CustomerServiceImpl implements CustomerService {
+
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        System.out.println("we are using setter injection");
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+}
