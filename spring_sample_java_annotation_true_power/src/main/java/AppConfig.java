2b8bbb256129eadfe652a95b2927ae4d81d7993c
KajanM <kajan.14@cse.mrt.ac.lk>
2017-08-07 11:46:13 +0530

    dependency injection | java configuration | annotation (autowire) | true power

---
+++ spring_sample_java_annotation_true_power/src/main/java/AppConfig.java
@@ -0,0 +1,17 @@
+import com.k4j4n.repository.CustomerRepository;
+import com.k4j4n.repository.HibernateCustomerRepositoryImpl;
+import com.k4j4n.service.CustomerService;
+import com.k4j4n.service.CustomerServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by K4J4N on 8/7/2017.
+ */
+
+@Configuration
+@ComponentScan({"com.k4j4n"})
+public class AppConfig {
+
+}
