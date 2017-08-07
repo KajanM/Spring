497a747ccdfbe95b2fb1469be0fa11dcfdc21a93
KajanM <kajan.14@cse.mrt.ac.lk>
2017-08-07 12:21:42 +0530

    bean scopes in spring

---
+++ spring_sample_scope/src/main/java/com/k4j4n/service/CustomerServiceImpl.java
@@ -0,0 +1,38 @@
+package com.k4j4n.service;
+
+import com.k4j4n.model.Customer;
+import com.k4j4n.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by K4J4N on 8/6/2017.
+ */
+
+@Service("customerService")
+//@Scope("singleton")
+@Scope("prototype")
+public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public CustomerServiceImpl() {
+        
+    }
+
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        System.out.println("we are using setter injection");
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+
+}
